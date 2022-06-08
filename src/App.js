dd47d891c19e9d5582127d92a02eddfbf9285240
Klym Andrii <klym.andriy14@gmail.com>
2022-06-08 12:00:49 +0300

    completed project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,72 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap';
-import Cards from './components/Cards/Cards';
-import Filters from './components/Filters/Filters';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
+
+import Search from "./components/Search/Search";
+import Card from "./components/Card/Card";
+import Pagination from "./components/Pagination/Pagination";
+import Filter from "./components/Filter/Filter";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CardDetails from "./components/Card/CardDetails";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-center fw-bold ubuntu my-4">rickandmortyapi test</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<CardDetails />} />
+      </Routes>
+    </Router>
+  );
+}
 
+const Home = () => {
+  let [pageNumber, updatePageNumber] = useState(1);
+  let [status, updateStatus] = useState("");
+  let [gender, updateGender] = useState("");
+  let [species, updateSpecies] = useState("");
+  let [fetchedData, updateFetchedData] = useState([]);
+  let [search, setSearch] = useState("");
+  let { info, results } = fetchedData;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
+  
+  return (
+    <div className="App">
+      <h1 className="text-center text-center fw-bold my-4">rickandmortyapi test</h1>
+      <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
       <div className="container">
         <div className="row">
-          <div className="col-3">
-            <Filters/>
-          </div>
-          <div className="col-8">
+          <Filter
+            pageNumber={pageNumber}
+            status={status}
+            updateStatus={updateStatus}
+            updateGender={updateGender}
+            updateSpecies={updateSpecies}
+            updatePageNumber={updatePageNumber}
+          />
+          <div className="col-lg-8 col-12">
             <div className="row">
-              <Cards/>
+              <Card page="/" results={results} />
             </div>
           </div>
         </div>
       </div>
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        updatePageNumber={updatePageNumber}
+      />
     </div>
   );
-}
+};
 
 export default App;
